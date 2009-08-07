801eb3751eefcc9fa728d0b6bc06e2cfb81537f7
okaya <okaya@localhost.localdomain>
2009-08-07 13:37:23 +0100

    t

---
+++ modules/gis2/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		alert("hello");
+	}
+	
+	public static void alert(Object message) {
+		System.out.println(message);
+		// javax.swing.JOptionPane.showMessageDialog(null, message);
+	}
+	public static double inputValue(String msg,String a)
+	{
+		boolean continueflag = true;
+		double tmpd = -1;
+		while(continueflag)
+		try{
+			tmpd = Double.parseDouble(JOptionPane.showInputDialog(msg,a));
+			continueflag = false;
+		}catch(Exception e){}
+		return tmpd;
+	}
+}
---
+++ modules/gis2/src/gis2/W.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class ClassName {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void alert(Object message) {
+		System.out.println(message);
+		// javax.swing.JOptionPane.showMessageDialog(null, message);
+	}
+}
